5fdf97df5d5d8f26e9e7235b9ecc80ba8f14e2aa
Carly Adams <carlybadams@yahoo.com>
2013-11-08 05:36:12 -0500

    Delete db_query.php

--- Libraries/db_query.php
+++
@@ -1,8 +0,0 @@
-# Our SQL command
-$q = "INSERT INTO users SET 
-    first_name = 'Sam', 
-    last_name = 'Seaborn',
-    email = 'seaborn@whitehouse.gov'";
-
-# Run the command
-echo DB::instance(DB_NAME)->query($q);
